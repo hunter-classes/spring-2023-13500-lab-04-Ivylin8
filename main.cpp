33eab2785e50be8006f571f5c221d81fee9af81a
Ivylin8 <104948596+Ivylin8@users.noreply.github.com>
2023-02-21 17:41:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main()
 {
     std::cout << "TASK A \n";
     std::cout << "Box width: 4, Box height: 8" <<'\n';
-    
     std::string a=box(4,8);
     std::cout << "-------------------------\n";
 
